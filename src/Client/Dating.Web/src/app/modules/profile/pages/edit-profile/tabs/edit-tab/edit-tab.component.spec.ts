6ee856f496c9997083c2203b44efd1515bc600e6
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2023-01-16 21:34:43 -0500

    fix

--- src/Client/Dating.Web/src/app/modules/profile/pages/edit-profile/tabs/edit-tab/edit-tab.component.spec.ts
+++ src/Client/Dating.Web/src/app/modules/profile/pages/edit-profile/tabs/edit-tab/edit-tab.component.spec.ts
@@ -1,6 +1,6 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
-
 import {EditTabComponent} from './edit-tab.component';
+
 
 describe('EditTabComponent', () => {
   let component: EditTabComponent;
